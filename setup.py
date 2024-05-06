cb63223a0348108b316d3c925bf9288a29a34a3f
Jakob van Santen <jvansanten@gmail.com>
2024-05-06 20:46:54 +0200

    Bump version to 2.0.0

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 
 setup(
     name = "pydashi",
-    version = "2.0.0a0",
+    version = "2.0.0",
     author = "The dashi developers",
     author_email = "eike@middell.net",
     python_requires = '>=3',
     description = ("TODO"),
     license = "LGPL",
     #keywords = "histograms fitting ",
-    url = "https://github.com/emiddell/dashi",
+    url = "https://github.com/IceCube-SPNO/dashi",
     packages=['dashi', 'dashi.tests', 'dashi.datasets'],
     long_description='TODO',
     classifiers=[
